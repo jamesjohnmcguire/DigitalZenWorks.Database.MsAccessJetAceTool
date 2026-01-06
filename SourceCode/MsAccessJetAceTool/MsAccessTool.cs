158c59e973a02da10eb2b92da5e420caff1130ad
James John McGuire <jamesjohnmcguire@gmail.com>
2026-01-06 16:48:59 +0900

    Encode Files as UTF-8 Without BOM

--- SourceCode/MsAccessJetAceTool/MsAccessTool.cs
+++ SourceCode/MsAccessJetAceTool/MsAccessTool.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="MsAccessTool.cs" company="James John McGuire">
+// <copyright file="MsAccessTool.cs" company="James John McGuire">
 // Copyright © 2006 - 2026 James John McGuire. All Rights Reserved.
 // </copyright>
 
