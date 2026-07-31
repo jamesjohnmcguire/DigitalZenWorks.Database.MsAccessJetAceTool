3f7e9f7db36320aa902452d45090904a8d09645b
JamesMc <jamesjohnmcguire@gmail.com>
2026-07-31 18:40:39 -0400

    Add Test Project

---
+++ SourceCode/MsAccessJetAceTool.Tests/UnitTests.cs
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////
+// <copyright file="UnitTests.cs" company="James John McGuire">
+// Copyright © 2006 - 2026 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace MsAccessJetAceTool.Tests;
+
+using NUnit.Framework;
+
+[TestFixture]
+internal class UnitTests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Test1()
+	{
+		Assert.Pass();
+	}
+}
